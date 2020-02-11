ffcd8da73fabd159c6e9ec3151d30783e3fed9c5
Bessy Gee <nakiwarai13@gmail.com>
2020-02-11 13:08:50 -0500

    created initial React app files

---
+++ defunctdisney/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <div className="container white">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>DefunctDisney.</b>
+        </p>
+    </div>
+    </header>
+    </div>
+  );
+}
+
+export default App;
