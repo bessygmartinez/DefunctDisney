ec481e95a68a0661db4e68e44702627b22a96e62
Bessy Gee <nakiwarai13@gmail.com>
2020-02-11 21:06:47 -0500

    refactured App to include Navbar component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import rides from './rides.json';
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <header className="App-header">
-      <div className="container white">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b>DefunctDisney.</b>
-        </p>
-    </div>
-    </header>
-    </div>
+   <div className="App">
+     <Navbar />
+     <header className="App-header" />
+     </div>
   );
 }
 
