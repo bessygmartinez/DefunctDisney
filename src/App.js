4be75a29d1c785b3a545e537837cf818aa2470c5
Bessy Gee <nakiwarai13@gmail.com>
2020-02-13 16:45:59 -0500

    added HowToPlayModal and Scoreboard components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 // import rides from './rides.json';
 import Navbar from './components/navbar';
+import HowToPlayModal from './components/howtoplaymodal';
+import Scoreboard from './components/scoreboard';
 import './App.css';
 
 function App() {
-  return (
-   <div className="App">
+  return ( 
+    <div className="transparent">
+     <HowToPlayModal />
      <Navbar />
-     <header className="App-header" />
-     </div>
+     <Scoreboard />     
+    </div> 
   );
 }
 
