bbf120a9df4d9855753e999984e727efe1249f2f
Bessy Gee <nakiwarai13@gmail.com>
2020-02-14 16:58:06 -0500

    added index.js, footer.js, and footer.css. Added css for footer styling

---
+++ src/components/footer/footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './footer.css'
+
+function Footer() {
+    return (
+        <footer className="page-footer footercr">
+        <div className="container">
+          <div className="row">
+          </div>
+        
+        <div className="footer-copyright footercr">
+          <div className="container text-white">
+          © 2020 Bessy G. Martinez
+          <a className="right githubicon" href="https://github.com/bessygmartinez/DefunctDisney" target="_new">
+              <i className="fab fa-github"></i>
+              </a>
+          </div>
+        </div></div>
+      </footer>
+    );
+};
+
+export default Footer;
