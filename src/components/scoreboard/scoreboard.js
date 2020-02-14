e9104aed43aea19eea5cf8c4d735f4106f5425ee
Bessy Gee <nakiwarai13@gmail.com>
2020-02-14 16:57:02 -0500

    added props set up in App.js to scores

--- src/components/scoreboard/scoreboard.js
+++ src/components/scoreboard/scoreboard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './scoreboard.css'
 
-function Scoreboard() {
+function Scoreboard(props) {
     return (
         <div className="row">
         <div className="col s6 offset-s3">
           <div className="card"> 
             <div className="card-content center">
               <div className="row">
-              <div className="col s12 m6"><b>Score: 0</b></div>
-              <div className="col s12 m6"><b>Top Score: 0</b></div>
+              <div className="col s12 m6"><b>Score: {props.score}</b></div>
+              <div className="col s12 m6"><b>Top Score: {props.topScore}</b></div>
               </div>
               </div>
           </div>
