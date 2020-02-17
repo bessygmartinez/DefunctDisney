7c1ae92c3c83d1a7cd446b19d1162b11659ba1b3
Bessy Gee <nakiwarai13@gmail.com>
2020-02-17 17:44:40 -0500

    added import for toast from materialize-css, corrected setState syntax, added toast to checkClick function for game over, added divs for better responsiveness and centering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Scoreboard from "./components/scoreboard";
 import RidesCard from './components/ridesCard';
 import Footer from "./components/footer";
 import "./App.css";
+import { toast } from "materialize-css";
 
 class App extends Component {
   state = {
@@ -21,11 +22,11 @@ class App extends Component {
     this.checkClick(name, this.updateTopScore);
   };
 
-  shuffleRides = () => {
-    this.setState((this.state.rides = this.shuffleRidesArray(this.staterides)));
+  shuffleRides = (rides) => {
+    this.setState(rides = this.shuffleRidesArray(this.state.rides));
   };
 
-  shuffleRidesArray = a => {
+  shuffleRidesArray = (a) => {
     let x, y, i;
     for (i = a.length - 1; i > 0; i--) {
       x = Math.floor(Math.random() * (i + 1));
@@ -39,7 +40,7 @@ class App extends Component {
   updateTopScore = (newState, cb) => {
     if (newState.ridesClicked.length > newState.topScore) {
       newState.topScore++;
-      this.setState((this.state = newState));
+      this.setState(newState);
     }
     cb(newState);
   };
@@ -48,27 +49,29 @@ class App extends Component {
     if (newState.ridesClicked.length === 12) {
       newState.message = `You Remembered! You Win!`;
       newState.ridesClicked = [];
-      this.setState((this.state = newState));
+      this.setState(newState);
     }
   };
 
   checkClick = (ride, cb) => {
     const newState = { ...this.state };
     if (newState.ridesClicked.includes(ride)) {
-      newState.message = `You already clicked on that one!`;
+      newState.message = `Game Over! You already clicked on ${ride}!`;
+      console.log(newState.message)
+      toast({html: `${newState.message}`, displayLength: 2000})
       newState.ridesClicked = [];
-      this.setState((this.state = newState));
+      this.setState(newState);
     } else {
       newState.ridesClicked.push(ride);
       newState.message = `Nice!`;
-      this.setState((this.state = newState));
+      this.setState(newState);
     }
     cb(newState, this.youWin);
   };
 
   render() {
     return (
-      <div className="transparent">
+      <div>
         <HowToPlayModal />
         <header>
           <Navbar />
@@ -79,7 +82,9 @@ class App extends Component {
         </header>
 
         <main>
-
+          <div className="row">
+          <div className="col l12 m12 s6 offset-s2">
+          <div className="ridesDiv">
           {this.state.rides.map(ride => (
           <RidesCard
             id={ride.id}
@@ -89,8 +94,12 @@ class App extends Component {
             handleClicked={this.handleClicked}
           />
           ))}
-          
+        <br style={{clear: "both"}} />
+        </div>
+        </div>
+        </div>
         </main>
+        
         <footer>
           <Footer />
         </footer>
