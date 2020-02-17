b40293010a0b92cc83cf8ec97c4135c4d1cdb8c9
Bessy Gee <nakiwarai13@gmail.com>
2020-02-17 17:46:13 -0500

    added outer div col classes for better centering, removed location, dateopened, dateclosed from card component

--- src/components/ridesCard/ridesCard.js
+++ src/components/ridesCard/ridesCard.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './ridesCard.css';
 
-const rideCard = ({ id, name, image, location, dateOpened, dateClosed, handleClicked }) => (
-    <div className="ridesDiv">
+const rideCard = ({ id, name, image, handleClicked }) => (
+        <div className="col l2 m4 s12 center">
         <div
             className="card"
             key={id}
             data-id={id}
             name={name}
             style={{ backgroundImage: `url(${image})` }}
-            location={location}
-            dateOpened = {dateOpened}
-            dateClosed = {dateClosed}
             onClick={handleClicked}
+            alt={name}
+            title={name}
         >
         </div>
-    </div>
+        </div>
 )
 
 export default rideCard;
