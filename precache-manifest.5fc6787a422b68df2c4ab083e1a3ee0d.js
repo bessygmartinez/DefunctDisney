7a35380e528a9155792c3d637d579c33fb596394
Bessy Gee <nakiwarai13@gmail.com>
2020-02-18 14:37:00 -0500

    Updates

---
+++ precache-manifest.5fc6787a422b68df2c4ab083e1a3ee0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0c254af44a007597f7f0be37895405",
+    "url": "/DefunctDisney/index.html"
+  },
+  {
+    "revision": "db84ae0b311d3007c75a",
+    "url": "/DefunctDisney/static/css/main.bb25e770.chunk.css"
+  },
+  {
+    "revision": "e3b388b82956520ae723",
+    "url": "/DefunctDisney/static/js/2.c51f2dbc.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "/DefunctDisney/static/js/2.c51f2dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db84ae0b311d3007c75a",
+    "url": "/DefunctDisney/static/js/main.aeed292c.chunk.js"
+  },
+  {
+    "revision": "d48993e74a2237de40f7",
+    "url": "/DefunctDisney/static/js/runtime-main.6e74032a.js"
+  },
+  {
+    "revision": "06255a529b509746d9dd3615d0392684",
+    "url": "/DefunctDisney/static/media/bg.06255a52.svg"
+  },
+  {
+    "revision": "72d802141f2c8aa8fa5df1e8acab2732",
+    "url": "/DefunctDisney/static/media/logo.72d80214.svg"
+  }
+]);
