91316ce07870b8dcb3a7315ec143f82790213e02
Bessy Gee <nakiwarai13@gmail.com>
2020-02-18 13:51:46 -0500

    added home link to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import HowToPlayModal from "../howtoplaymodal";
 import logo from '../../logo.svg'
 import './navbar.css'
 
 function Navbar() {
     return (
         <div className="navbar-fixed">
+          <HowToPlayModal />
       <nav>
       <div className="nav-wrapper white nav-text">    
-        <a href="index"><img src={logo} className="brand-logo right App-logo heartbeat" alt="retro mickey ears" /></a>
-        <a href="index" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <Link to="/"><img src={logo} className="brand-logo right App-logo heartbeat" alt="retro mickey ears" /></Link>
+        <Link to="/" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
 
           <ul id="nav-mobile" className="left hide-on-med-and-down">              
-            <li><b className="col s4"><h3><b>DefunctDisney</b></h3></b></li>
+            <li><Link to="/" className="col s4" style={{color: "#3c334a"}}><h3><b>DefunctDisney</b></h3></Link></li>
             <li><div id="tagline">A memory game featuring defunct Walt Disney World rides.</div></li>
+            <li><a className="navpadding waves-effect waves-light" href="/">Home</a></li>
             <li><a className="navpadding waves-effect waves-light modal-trigger" href="#how-to-play">How to Play</a></li>
-            <li><a className="navpadding waves-effect waves-light" href="./rides.json">About the Rides</a></li>
+            <li><a className="navpadding waves-effect waves-light" href="/AboutRides">About the Rides</a></li>
         </ul>
       </div>
     </nav>
 
 
     <ul className="sidenav" id="mobile-nav">
-    <li><h4><b>DefunctDisney</b></h4></li>
+    <li><Link to="/" style={{color: "#3c334a"}}><h4><b>DefunctDisney</b></h4></Link></li>
         <li><div id="tagline-mobile">A memory game featuring defunct Walt Disney World rides.</div></li>
+        <li><a className="waves-effect waves-light" href="/">Home</a></li>
         <li><a className="waves-effect waves-light modal-trigger" href="#how-to-play">How to Play</a></li>
-        <li><a className="waves-effect waves-light" href="rides.html">About the Rides</a></li>
+        <li><Link to="/AboutRides" className="waves-effect waves-light">About the Rides</Link></li>
   </ul>
     </div>  
     )
