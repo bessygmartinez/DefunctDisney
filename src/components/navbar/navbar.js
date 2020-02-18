7638ca285f89600557cdd41db0bf189dac55f7df
Bessy Gee <nakiwarai13@gmail.com>
2020-02-18 15:21:49 -0500

    changed Link to DefunctDisney for home

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,13 +10,13 @@ function Navbar() {
           <HowToPlayModal />
       <nav>
       <div className="nav-wrapper white nav-text">    
-        <Link to="/"><img src={logo} className="brand-logo right App-logo heartbeat" alt="retro mickey ears" /></Link>
-        <Link to="/" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+        <Link to="/DefunctDisney"><img src={logo} className="brand-logo right App-logo heartbeat" alt="retro mickey ears" /></Link>
+        <Link to="/DefunctDisney" data-target="mobile-nav" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
 
           <ul id="nav-mobile" className="left hide-on-med-and-down">              
-            <li><Link to="/" className="col s4" style={{color: "#3c334a"}}><h3><b>DefunctDisney</b></h3></Link></li>
+            <li><Link to="/DefunctDisney" className="col s4" style={{color: "#3c334a"}}><h3><b>DefunctDisney</b></h3></Link></li>
             <li><div id="tagline">A memory game featuring defunct Walt Disney World rides.</div></li>
-            <li><a className="navpadding waves-effect waves-light" href="/">Home</a></li>
+            <li><a className="navpadding waves-effect waves-light" href="/DefunctDisney">Home</a></li>
             <li><a className="navpadding waves-effect waves-light modal-trigger" href="#how-to-play">How to Play</a></li>
             <li><a className="navpadding waves-effect waves-light" href="/AboutRides">About the Rides</a></li>
         </ul>
