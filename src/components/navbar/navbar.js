4ad964960a23fcb05d88657e5ae2b6b1adb252b2
Bessy Gee <nakiwarai13@gmail.com>
2020-02-19 15:01:17 -0500

    corrected Home link to <Link to=> syntax

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
           <ul id="nav-mobile" className="left hide-on-med-and-down">              
             <li><Link to="/" className="col s4" style={{color: "#3c334a"}}><h3><b>DefunctDisney</b></h3></Link></li>
             <li><div id="tagline">A memory game featuring defunct Walt Disney World rides.</div></li>
-            <li><a className="navpadding waves-effect waves-light" href="/">Home</a></li>
+            <li><Link to="/" className="navpadding waves-effect waves-light">Home</Link></li>
             <li><a className="navpadding waves-effect waves-light modal-trigger" href="#how-to-play">How to Play</a></li>
             <li><a className="navpadding waves-effect waves-light" href="#/AboutRides">About the Rides</a></li>
         </ul>
@@ -27,7 +27,7 @@ function Navbar() {
     <ul className="sidenav" id="mobile-nav">
     <li><Link to="/" style={{color: "#3c334a"}}><h4><b>DefunctDisney</b></h4></Link></li>
         <li><div id="tagline-mobile">A memory game featuring defunct Walt Disney World rides.</div></li>
-        <li><a className="waves-effect waves-light" href="/">Home</a></li>
+        <li><Link to="/" className="waves-effect waves-light">Home</Link></li>
         <li><a className="waves-effect waves-light modal-trigger" href="#how-to-play">How to Play</a></li>
         <li><Link to="#/AboutRides" className="waves-effect waves-light">About the Rides</Link></li>
   </ul>
